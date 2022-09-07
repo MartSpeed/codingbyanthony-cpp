3f0f0bdf4de4c25ff01bc6f7b084cebb968b622a
John Anthony <jmanthony.fullsail@gmail.com>
2022-09-07 11:25:11 -0500

    FEAT: cpp array length and manipulation practice

--- datatypes/Sources/arrays.cpp
+++ datatypes/Sources/arrays.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
- // i am an array and i want to be added to the repo with my friends
+  // array practice
+  int luckyNums[6] = {4, 8 , 15, 16, 23, 42};
+  cout << sizeof(luckyNums) << endl;
+  // getting the length of the array
+  // an array length in c++ is shown in bytes
+  // to get how many elements inside of an array you need to divide the size of variable by the size of the integers variable
+  int getArrayLength = sizeof(luckyNums) / sizeof(int);
+  cout << getArrayLength;
+
   return 0;
 }
